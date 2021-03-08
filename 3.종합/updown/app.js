156fca7caefd17864fc19a17b75353ea9fdca04b
felizju <felizju@gmail.com>
2021-03-08 13:48:46 +0900

    [21.03.08] #UpDown게임 작성 완료

--- 3.종합/updown/app.js
+++ 3.종합/updown/app.js
@@ -27,35 +27,62 @@ function makeNumberIcons(){
 
 
 function clearNumberIcons($numbers){
-    
     for (let $icon of [...$numbers.children]) {
         $numbers.removeChild($icon);
     }
 }
 
 
+// Up&Down 애니메이션을 작동시킬 클래스 추가/제거 함수 정의
+function executeUpDownAnimation(isUp){
+    const ANI_CLASS_NAME = 'selected';
+    document.getElementById('up').classList.toggle('ANI_CLASS_NAME', isUp);
+    document.getElementById('down').classList.toggle('ANI_CLASS_NAME', !isUp);
+}
+
+
+// 정답을 맞췄을 때 처리를 수행할 함수 정의
+function prcessCorrect($target){
+    // 축하 메시지 박스 나타나게 하는 코드
+    const $finish = document.getElementById('finish');
+    $finish.classList.add('show');
+
+    // 정답 아이콘 움직이게 하는 코드
+    $target.setAttribute('id', 'move');
+}
+
 // 정답을 판별해주는 함수 정의
-function checkAnwser($numbers){
+function checkAnwser($numbers, $target){
+
     // 객체 디스트럭처링
     const{secret, answer} = gameDatas;
-
+    
     const $begin = document.getElementById('begin');
     const $end = document.getElementById('end');
-
+    
+    // const $up = document.getElementById('up');
+    // const $down = document.getElementById('down');
 
     if(secret === answer){
-        congraturation();
+        // 정답처리 수행 함수
+        prcessCorrect($target);
+        return;
 
     }else if(secret < answer){
         // down인 경우
         gameDatas.max = answer-1;
         $end.textContent = answer-1;
-        myJumpingDown();
+        // $up.classList.remove('selected');
+        // $down.classList.add('selected');
+        executeUpDownAnimation(false);
+
     }else{
         // up인 경우
         gameDatas.min = answer+1;
         $begin.textContent = answer+1;
-        myJumpingUp();
+        // $up.classList.add('selected');
+        // $down.classList.remove('selected');
+        executeUpDownAnimation(true);
     }
     // console.log(gameDatas);
 
@@ -64,27 +91,6 @@ function checkAnwser($numbers){
     makeNumberIcons(); //min, max 변화대로 재배치
 }
 
-
-// congraturation
-function congraturation(){
-    const $finish = document.getElementById('finish');
-    gameDatas.secret.addEventListener('click', function(){
-        $finish.className = 'selected';
-    });
-}
-
-// 애니메이션 up
-function myJumpingUp(){
-    const $up = document.getElementById('up');
-    $up.className = 'selected';
-}
-
-
-// 애니메이션 down
-function myJumpingDown(){
-    const $down = document.getElementById('down');
-    $down.className = 'selected';
-}
 
 
 // 핵심 실행 로직 즉시 실행 함수
@@ -101,6 +107,6 @@ function myJumpingDown(){
         // console.log(gameDatas);
     
         // 정답 체크 함수 호출
-        checkAnwser($numbers);
+        checkAnwser($numbers, e.target);
     });
 }());
