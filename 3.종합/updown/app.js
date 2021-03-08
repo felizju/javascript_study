99dfaf837564daa095da844acdf0f3cd7f7021b6
felizju <felizju1112@gmail.com>
2021-03-08 11:39:01 +0900

    [21.03.08] #과제

--- 3.종합/updown/app.js
+++ 3.종합/updown/app.js
@@ -1,7 +1,7 @@
 
 // 게임에서 사용할 데이터 객체
 const gameDatas = {
-    secret : Math.floor(Math.random()*100)+1,
+    secret : Math.floor(Math.random() * 100) + 1,
     answer : null, // 사용자가 클릭한 숫자
     min : 1,
     max : 100
@@ -14,14 +14,13 @@ function makeNumberIcons(){
     const $numbers = document.getElementById('numbers');
     const $frag = document.createDocumentFragment();    // 성능을 위한 가상 DOM
 
-    for(let i=gameDatas.min; i<=gameDatas.max; i++){
+    for(let i = gameDatas.min; i <= gameDatas.max; i++){
         const $icon = document.createElement('div');
         $icon.classList.add('icon');
         $icon.textContent = i;
         $frag.appendChild($icon);
     }
     $numbers.appendChild($frag);
-
     return $numbers;
 }
 
@@ -32,7 +31,6 @@ function clearNumberIcons($numbers){
     for (let $icon of [...$numbers.children]) {
         $numbers.removeChild($icon);
     }
-
 }
 
 
@@ -45,19 +43,19 @@ function checkAnwser($numbers){
     const $end = document.getElementById('end');
 
 
-
     if(secret === answer){
-                
+        congraturation();
 
     }else if(secret < answer){
         // down인 경우
         gameDatas.max = answer-1;
         $end.textContent = answer-1;
-
+        myJumpingDown();
     }else{
         // up인 경우
         gameDatas.min = answer+1;
         $begin.textContent = answer+1;
+        myJumpingUp();
     }
     // console.log(gameDatas);
 
@@ -66,6 +64,27 @@ function checkAnwser($numbers){
     makeNumberIcons(); //min, max 변화대로 재배치
 }
 
+
+// congraturation
+function congraturation(){
+    const $finish = document.getElementById('finish');
+    gameDatas.secret.addEventListener('click', function(){
+        $finish.className = 'selected';
+    });
+}
+
+// 애니메이션 up
+function myJumpingUp(){
+    const $up = document.getElementById('up');
+    $up.className = 'selected';
+}
+
+
+// 애니메이션 down
+function myJumpingDown(){
+    const $down = document.getElementById('down');
+    $down.className = 'selected';
+}
 
 
 // 핵심 실행 로직 즉시 실행 함수
@@ -78,13 +97,10 @@ function checkAnwser($numbers){
             return;
         }
         // console.log(e.target.textContent);
-        
         gameDatas.answer = +e.target.textContent;
         // console.log(gameDatas);
     
         // 정답 체크 함수 호출
         checkAnwser($numbers);
     });
-
-
 }());
